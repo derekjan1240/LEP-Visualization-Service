157390d2badd7e3aa9d7b6dec2574d04ec2ebc9b
Kai-Jian Zhan (Derek) <derekjan1240@gmail.com>
2021-06-19 11:31:46 +0800

    新增 Redis

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,41 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const micro = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.REDIS,
+      options: {
+        url: 'redis://localhost:6379',
+        retryAttempts: 5, // 對外request重試次數
+        retryDelay: 1000, // 重試間隔
+      },
+    },
+  );
+  micro.listen(() => {
+    Logger.log('Microservice is listening!');
+  });
+
+  const logger = new Logger('Bootstrap');
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: 'http://localhost:3000',
+      credentials: true,
+    },
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
   await app.listen(8086, () => {
-    Logger.log('Visualization Service is running on port 8086!');
+    logger.log('Visualization Service is running on port 8086!');
   });
 }
 bootstrap();
