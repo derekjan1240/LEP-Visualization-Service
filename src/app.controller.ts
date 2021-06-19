684e4213ed458e77e906d751517850763cfa8b2c
Kai-Jian Zhan (Derek) <derekjan1240@gmail.com>
2021-06-19 11:33:35 +0800

    新增 測試 API

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @MessagePattern('VISUALIZATION_test')
+  async getContentTest(id: number): Promise<any> {
+    const FAKE_VISUALIZATION_DATA = [
+      {
+        id: '1',
+        name: 'FAKE_VISUALIZATION_DATA',
+      },
+    ];
+    return FAKE_VISUALIZATION_DATA[id];
+  }
+
   @Get()
   test(): string {
     return '[Visualization Service] : OK!';
+  }
+
+  @Post('test')
+  public async visualizationTest(@Body() body: any) {
+    const FAKE_VISUALIZATION_DATA = [
+      {
+        id: '1',
+        name: 'FAKE_VISUALIZATION_DATA',
+      },
+    ];
+    return FAKE_VISUALIZATION_DATA[body.data.id];
   }
 }
